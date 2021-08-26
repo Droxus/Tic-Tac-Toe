5e8df4c24e6e62b4e19533761d9803ea64cff73a
SAnya Sanyaw <alexredov@gmail.com>
2021-08-27 00:34:36 +0300

    added stackLine

--- script.js
+++ script.js
@@ -20,10 +20,15 @@ function onClick(event) {
     })
     use.style.strokeDasharray = 1000
 
+
     fillCellsCounter++
     isXturn = !isXturn
     checkWinner()
 }
+// const dyline = document.createElement('svg')
+// document.body.appendChild(dyline)
+// document.createElement('line')
+console.log()
 
 function checkWinner() {
     for (let i = 0; i < 9; i += 3) {
@@ -31,6 +36,22 @@ function checkWinner() {
             uses[i].getAttribute('href') === uses[i + 1].getAttribute('href') &&
             uses[i + 1].getAttribute('href') === uses[i + 2].getAttribute('href')
         if (isColumnSet) {
+            let coordOFsvg = 0
+            switch (i) {
+                case 0:  coordOFsvg=16; break;
+                case 3:  coordOFsvg=50; break;
+                case 6:  coordOFsvg=82; break;
+            }
+            const grid = uses[i].parentElement.parentElement
+            console.log(grid)
+            const top = grid.offsetTop
+            const left = grid.offsetLeft
+            const width = grid.offsetWidth
+            const height = grid.offsetHeight
+            document.body.innerHTML += `
+            <svg style="top:${top}px; left:${left}px; position: absolute;" width="${width}" height="${height}">
+                <line x1="0%" y1="${coordOFsvg}%" x2="100%" y2="${coordOFsvg}%" stroke-width="2%" stroke="black" />
+            </svg>`
             anounceWinner()
             clearAllCells()
             return
@@ -42,16 +63,60 @@ function checkWinner() {
             uses[i].getAttribute('href') === uses[i + 3].getAttribute('href') &&
             uses[i + 3].getAttribute('href') === uses[i + 6].getAttribute('href')
         if (isRowSet) {
+            let coordOFsvg = 0
+            switch (i) {
+                case 0:  coordOFsvg=16; break;
+                case 1:  coordOFsvg=50; break;
+                case 2:  coordOFsvg=82; break;
+            }
+
+            const grid = uses[i].parentElement.parentElement
+            console.log(grid)
+            const top = grid.offsetTop
+            const left = grid.offsetLeft
+            const width = grid.offsetWidth
+            const height = grid.offsetHeight
+            document.body.innerHTML += `
+            <svg style="top:${top}px; left:${left}px; position: absolute;" width="${width}" height="${height}">
+                <line x1="${coordOFsvg}%" y1="0%" x2="${coordOFsvg}%" y2="100%" stroke-width="2%" stroke="black" />
+            </svg>`
+
             anounceWinner()
             clearAllCells()
             return
         }
     }
     if (uses[4].getAttribute('href') &&
-        (uses[2].getAttribute('href') === uses[4].getAttribute('href') &&
-            uses[4].getAttribute('href') === uses[6].getAttribute('href') ||
+        uses[2].getAttribute('href') === uses[4].getAttribute('href') &&
+        uses[4].getAttribute('href') === uses[6].getAttribute('href')) {
+            const grid = uses[4].parentElement.parentElement
+            console.log(grid)
+            const top = grid.offsetTop
+            const left = grid.offsetLeft
+            const width = grid.offsetWidth
+            const height = grid.offsetHeight
+            document.body.innerHTML += `
+            <svg style="top:${top}px; left:${left}px; position: absolute;" width="${width}" height="${height}">
+                <line x1="0%" y1="100%" x2="100%" y2="0%" stroke-width="2%" stroke="black" />
+            </svg>`
+
+                anounceWinner()
+                clearAllCells()
+                return               
+            }
+            if  ( uses[4].getAttribute('href') && 
             uses[0].getAttribute('href') === uses[4].getAttribute('href') &&
-            uses[4].getAttribute('href') === uses[8].getAttribute('href'))) {
+            uses[4].getAttribute('href') === uses[8].getAttribute('href')) {
+                const grid = uses[4].parentElement.parentElement
+                console.log(grid)
+                const top = grid.offsetTop
+                const left = grid.offsetLeft
+                const width = grid.offsetWidth
+                const height = grid.offsetHeight
+                document.body.innerHTML += `
+                <svg style="top:${top}px; left:${left}px; position: absolute;" width="${width}" height="${height}">
+                    <line x1="0%" y1="0%" x2="100%" y2="100%" stroke-width="2%" stroke="black" />
+                </svg>`
                 anounceWinner()
                 clearAllCells()
                 return
@@ -72,7 +137,9 @@ function clearAllCells() {
 }
 
 function anounceWinner() { 
-    alert("Winner " + (!isXturn ? 'X' : 'O') + "!")
+    
+setTimeout( e=> alert("Winner " + (!isXturn ? 'X' : 'O') + "!"), 3000)
+    
 }
 
 const cells = Array.from(document.getElementsByClassName('cell'));
